92a9fdab0207783f4806664110278e210e1fd673
reza soleymani <rezasoleiii@gmail.com>
2025-11-15 01:53:19 +0330

    add page 8

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import Dashboard from "./pages/Dashboard";
 import AccountManagement from './pages/AccountManagement'
 import Paymentpage from './pages/Paymentpage'
 import BillingServices from './pages/BillingServices'
+import Charging from './pages/Charging'
+import ChangePassword from './pages/ChangePassword'
+import RequestCard from './pages/RequestCard'
 
 function App() {
 
 
   return (
       <div>
-        <BillingServices />
+        <RequestCard />
       </div>
   )
 }
